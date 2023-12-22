0b144e4086ef38302add9332dafd12a464f2b699
soledad <soledad@smartescrow.es>
2023-12-22 17:46:49 +0100

    addGetOrders

--- src/Controller/ShopifyController.php
+++ src/Controller/ShopifyController.php
@@ -2,63 +2,143 @@
 
 namespace App\Controller;
 
+use GuzzleHttp\Client;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class ShopifyController extends AbstractController
 {
-    /**
-     * @Route("/shopify", name="shopify")
-     */
-    public function authorizeShopify(): RedirectResponse
+    #[Route('/shopify', name: 'app_shopify')]
+    public function getProducts(): Response
     {
-        // Configuración para Shopify
-        $config = array(
-            'ShopUrl' => 'tiendapruebala.myshopify.com',
-            'ApiKey' => '5d3752ee6d2b47e8836ee3c7de699f0b',
-            'SharedSecret' => 'e3a80914f1b52bcd4786d82f370759fb',
-        );
+        $apiKey = '9b09ce6489ba709cd9894bf9287039f0';
+        $apiSecret = '0726d6ed5d891b5b14f1b711a83a7ecf';
+        $accessToken = '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';
 
-        // Configurar SDK de Shopify
-        \PHPShopify\ShopifySDK::config($config);
+        // Combinar las credenciales y codificarlas en Base64 para la autenticación básica
+        $credentials = base64_encode("$apiKey:$apiSecret");
 
-        // Definir los permisos que necesitas
-        $scopes = 'read_products,write_products,read_script_tags,write_script_tags';
+        // Crear un cliente Guzzle
+        $client = new Client();
 
-        // URL de redireccionamiento
-        $redirectUrl = $this->generateUrl('shopify_redirect_url', [], \Symfony\Component\Routing\Generator\UrlGeneratorInterface::ABSOLUTE_URL);
+        // Definir las cabeceras para la solicitud
+        $headers = [
+            'Authorization' => 'Basic ' . $credentials,
+            'X-Shopify-Access-Token' => $accessToken
+        ];
 
-        // Crear la solicitud de autenticación
-        \PHPShopify\AuthHelper::createAuthRequest($scopes, $redirectUrl);
+        $shopName = 'probando2023';
+        $apiVersion = '2021-04';
+        $resource = 'orders';
 
-        // Redirigir automáticamente al usuario a la página de autorización de Shopify
-        return $this->redirectToRoute('shopify_redirect_url');
-    }
+        // Realizar la solicitud GET a la API de Shopify
+        $response = $client->request('GET', 'https://' . $shopName . '.myshopify.com/admin/api/' . $apiVersion . '/' . $resource . '.json', [
+            'headers' => $headers
+        ]);
 
-    /**
-     * @Route("/shopify/redirect-url", name="shopify_redirect_url")
-     */
-    public function handleRedirect(Request $request): Response
-    {
-        // Configuración para Shopify
-        $config = array(
-            'ShopUrl' => 'tiendapruebala.myshopify.com',
-            'ApiKey' => '5d3752ee6d2b47e8836ee3c7de699f0b',
-            'SharedSecret' => 'e3a80914f1b52bcd4786d82f370759fb',
-        );
+        // Convertir la respuesta JSON en un objeto PHP
+        $data = json_decode($response->getBody(), true);
 
-        // Configurar SDK de Shopify
-        \PHPShopify\ShopifySDK::config($config);
+        $orders = $data['orders'];
+        $ordersTraducidos = [];
 
-        // Obtener el token de acceso cuando se redirige de nuevo a la autorización posterior a la aplicación
-        $accessToken = \PHPShopify\AuthHelper::getAccessToken();
+        foreach ($orders as $order) {
+            $lineItemsTraducidos = [];
+            //Itero sobre los detalles de cada uno de los productos del pedido
+            foreach ($order['line_items'] as $lineItem) {
+                $lineItemTraducido = [
+                    'ID' => $lineItem['id'],
+                    'Gramos' => $lineItem['grams'],
+                    'Nombre' => $lineItem['name'],
+                    'Precio' => $lineItem['price'],
+                    'ID Producto' => $lineItem['product_id'],
+                    'Cantidad' => $lineItem['quantity'],
+                    'SKU' => $lineItem['sku'],
+                    'Título' => $lineItem['title'],
+                    'Descuento Total' => $lineItem['total_discount'],
+                ];
+                array_push($lineItemsTraducidos, $lineItemTraducido);
+            }
 
-        // Almacenar el token de acceso en la base de datos o en algún otro lugar seguro
-        // ... Puedes implementar la lógica de almacenamiento aquí
+            //Hay 2 pero no lo itero y lo tomo directo porque es el mismo que se repite
+            $totalShipping = null;
+            if (isset($order['total_shipping_price_set']['shop_money'])) {
+                $totalShipping = [
+                    'Importe' => $order['total_shipping_price_set']['shop_money']['amount'],
+                    'Moneda' => $order['total_shipping_price_set']['shop_money']['currency_code'],
+                ];
+            }
 
-        return new Response('Token de acceso obtenido con éxito. Almacenado en la base de datos.');
+            $clienteTraducido = null;
+            if (isset($order['customer'])) {
+                $cliente = $order['customer'];
+                $direccionPredeterminadaTraducida = [];
+
+                // Verificar si 'default_address' existe en 'customer'
+                if (isset($cliente['default_address'])) {
+                    $direccionPredeterminada = $cliente['default_address'];
+                    $direccionPredeterminadaTraducida = [
+                        'ID' => $direccionPredeterminada['id'],
+                        'Nombre_Completo' => $direccionPredeterminada['name'],
+                        'Empresa' => $direccionPredeterminada['company'],
+                        'Dirección' => $direccionPredeterminada['address1'],
+                        'Ciudad' => $direccionPredeterminada['city'],
+                        'Provincia' => $direccionPredeterminada['province'],
+                        'País' => $direccionPredeterminada['country'],
+                        'Código Postal' => $direccionPredeterminada['zip'],
+                    ];
+                }
+                $clienteTraducido = [
+                    'ID' => $cliente['id'],
+                    'Correo_Electrónico' => $cliente['email'],
+                    'Nombre' => $cliente['first_name'],
+                    'Apellido' => $cliente['last_name'],
+                    'Estado' => $cliente['state'],
+                    'Exento_Impuestos' => $cliente['tax_exempt'],
+                    'Teléfono' => $cliente['phone'],
+                    'Moneda' => $cliente['currency'],
+                    'Exenciones_Impuestos' => $cliente['tax_exemptions'],
+                    'Dirección_Predeterminada' => $direccionPredeterminadaTraducida,
+
+                ];
+            }
+            // Devuelve todos los pedidos
+            $orderTraducido = [
+                'ID' => $order['id'],
+                'ID Pago' => $order['checkout_id'],
+                'Confirmado' => $order['confirmed'],
+                'Fecha Pedido' => $order['created_at'],
+                'Moneda' => $order['currency'],
+                'Subtotal Actual' => $order['current_subtotal_price'],
+                'Descuentos Totales Actuales' => $order['current_total_discounts'],
+                'Precio Total Actual' => $order['current_total_price'],
+                'Impuesto Total Actual' => $order['current_total_tax'],
+                'Estado Financiero del Pedido' => $order['financial_status'],
+                'Pasarela Pago' => $order['gateway'],
+                'Número Pedido' => $order['order_number'],
+                'URL Estado Pedido' => $order['order_status_url'],
+                'Moneda Presentación' => $order['presentment_currency'],
+                'Método Procesamiento' => $order['processing_method'],
+                'Precio Subtotal' => $order['subtotal_price'],
+                'Etiquetas' => $order['tags'],
+                'Impuestos Incluidos' => $order['taxes_included'],
+                'Descuentos_Totales' => $order['total_discounts'],
+                'Precio Total Artículos' => $order['total_line_items_price'],
+                'Total Pendiente' => $order['total_outstanding'],
+                'Precio Total' => $order['total_price'],
+                'Impuesto Total' => $order['total_tax'],
+                'Peso Total' => $order['total_weight'],
+                'Cliente' => $clienteTraducido,
+                'Artículos_Línea' => $lineItemsTraducidos,
+                'Precio Envío Total' => $totalShipping,
+            ];
+
+            array_push($ordersTraducidos, $orderTraducido);
+        }
+
+        return new Response(json_encode($ordersTraducidos, JSON_PRETTY_PRINT), Response::HTTP_OK, ['Content-Type' => 'application/json']);
     }
 }
--- src/Service/ShopifyAuthorizationService.php
+++ src/Service/ShopifyAuthorizationService.php
@@ -4,16 +4,17 @@ namespace App\Service;
 use PHPShopify\AuthHelper;
 use PHPShopify\ShopifySDK;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class ShopifyAuthorizationService
 {
-    public function authorizeAndRedirect(): Response
+    public function authorizeShopify(): RedirectResponse
     {
         // Configuración para Shopify
         $config = array(
-            'ShopUrl' => 'tiendapruebala.myshopify.com',
-            'ApiKey' => '5d3752ee6d2b47e8836ee3c7de699f0b',
-            'SharedSecret' => 'e3a80914f1b52bcd4786d82f370759fb',
+            'ShopUrl' => 'probando2023.myshopify.com',
+            'ApiKey' => '9b09ce6489ba709cd9894bf9287039f0',
+            'SharedSecret' => '0726d6ed5d891b5b14f1b711a83a7ecf',
         );
 
         // Configurar SDK de Shopify
@@ -22,11 +23,36 @@ class ShopifyAuthorizationService
         // Definir los permisos que necesitas
         $scopes = 'read_products,write_products,read_script_tags,write_script_tags';
 
+        // URL de redireccionamiento
+        $redirectUrl = 'https://d6r6nb2p-62558.uks1.devtunnels.ms/';
+
+        // Crear la solicitud de autenticación
+        \PHPShopify\AuthHelper::createAuthRequest($scopes, $redirectUrl);
+
+        // Redirigir automáticamente al usuario a la página de autorización de Shopify
+        return new RedirectResponse('/shopify/redirect-url');
+    }
+
+    /**
+     * @Route("/shopify/redirect-url", name="shopify_redirect_url")
+     */
+    public function handleRedirect(): Response
+    {
+        // Configuración para Shopify
+        $config = array(
+            'ShopUrl' => 'probando2023.myshopify.com',
+            'ApiKey' => '9b09ce6489ba709cd9894bf9287039f0',
+            'SharedSecret' => '0726d6ed5d891b5b14f1b711a83a7ecf',
+        );
+
+        // Configurar SDK de Shopify
+        \PHPShopify\ShopifySDK::config($config);
+
         // Obtener el token de acceso cuando se redirige de nuevo a la autorización posterior a la aplicación
-        $accessToken = \PHPShopify\AuthHelper::createAuthRequest($scopes);
-        
+        $accessToken = \PHPShopify\AuthHelper::getAccessToken();
+
         // Almacenar el token de acceso en la base de datos o en algún otro lugar seguro
-        // ... Puedes implementar la lógica de almacenamiento aquí    
+        // ... Puedes implementar la lógica de almacenamiento aquí
 
         return new Response('Token de acceso obtenido con éxito. Almacenado en la base de datos.');
     }
